1bb16943e42955110d17ebdbce92a3431c1ebc43
OTR <49165533+otr@users.noreply.github.com>
2024-07-31 19:57:44 +0600

    [CHORE] Add static fiels for bootstrap frontend framework

--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@ import os
 import sys
 
 
-def main():
+def main(settings):
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,4 +18,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    path_to_settings = os.environ.get(
+        "DJANGO_SETTINGS_MODULE",
+        "config.settings.test_settings",
+    )
+    main(path_to_settings)
