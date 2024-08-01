1ff9affccd7ac5c49684a3ca018bb48e6dd02ed8
OTR <49165533+otr@users.noreply.github.com>
2024-08-01 12:38:19 +0600

    [FEAT] Add python-anywhere settings

---
+++ config/settings/paw_settings.py
@@ -0,0 +1,38 @@
+"""
+Concrete production settings for `pythonanywhere.com`, extend base production
+ settings from `prod_settings.py`
+
+Variables with leading "U_" mean user defined settings, not Django's
+"""
+import os
+
+from config.settings.prod_settings import *
+
+# Your login on hosting
+U_LOGIN = os.environ.get("LOGIN")
+U_DB_PASSWD = os.environ.get("DB_PASSWD")
+# MySQL table name, creates on a dashboard
+U_TABLE_NAME = "test_targets"
+
+ALLOWED_HOSTS = [f"{U_LOGIN}.pythonanywhere.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": f"{U_LOGIN}${U_TABLE_NAME}",
+        "HOST": f"{U_LOGIN}.mysql.pythonanywhere-services.com",
+        "USER": f"{U_LOGIN}",
+        "PASSWORD": f"{U_DB_PASSWD}",
+    },
+}
+
+# TODO: Use ENVIRONMENT VARIABLE instead of hardcoded timezone
+TIME_ZONE = "Etc/GMT-3"  # UTC+3
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_ROOT = f"/home/{U_LOGIN}/social_parser/static"
+STATIC_DIR = BASE_DIR / "static"
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_URL = "/static/"
---
+++ config/settings/prod_settings.py
@@ -0,0 +1,25 @@
+"""
+Base settings for production use, DO NOT USE IT AS IS, any concrete
+production settings should extend this settings.
+
+See: https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+"""
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+from config.settings.common_settings import *
+
+
+def change_it() -> None:
+    """
+    Run `python -c 'from django.core.management.utils import
+    get_random_secret_key; print(get_random_secret_key())'`
+    to generate a new secret key.
+    """
+    raise ImproperlyConfigured("The SECRET_KEY setting must not be empty.")
+
+
+ALLOWED_HOSTS = []
+DEBUG = False
+SECRET_KEY = os.getenv("SECRET_KEY") or change_it()
