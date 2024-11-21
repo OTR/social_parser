1fdb04b4070abc01a7164f1627a74e4004c1cef1
OTR <49165533+OTR@users.noreply.github.com>
2024-11-21 11:35:34 +0600

    [FIX] Add STATIC_ROOT variable

--- config/settings/test_settings.py
+++ config/settings/test_settings.py
@@ -4,8 +4,7 @@ from config.settings.common_settings import *
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
-STATIC_DIR = BASE_DIR / "static"
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
 
-TIME_ZONE = "Etc/GMT-6"  # UTC+6
+TIME_ZONE = "Etc/GMT-3"  # UTC+3
